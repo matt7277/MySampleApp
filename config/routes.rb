70ef3ea9c76df43ed5d439a6d67a78b89b64d6da
Matthew Saeed <saeedm@amazon.com>
2013-10-13 20:07:15 -0700

    8.2.2 A working sign_in method

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 SampleApp::Application.routes.draw do
-  root to: 'static_pages#home'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'  
-  resources :users
-
-
+  match '/signin', to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
   #get "static_pages/about"
   #get "static_pages/contact"
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,12 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com",
                    password: "foobar", password_confirmation: "foobar")
 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   end
+
+
 
 describe "when email is not present" do
     before { @user.email = " " }
@@ -95,6 +100,10 @@ end
 
   it { should be_valid }
 
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+end
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
